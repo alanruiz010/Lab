b56b8b8b6468686767d10a5210f14cceb82b4eec
alanruiz010 <alan.ruiz010@hotmail.com>
2017-10-11 00:24:04 -0300

    subiendo tp sin terminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';//FORMBUILDER CREA FORMS, FORMGROUP DEFINE UN FORMULARIO Y VALIDATORS CONTIENE VALIDACIONES PREDISEÑADAS
+//import { PromptComponent } from './prompt.component';
+import { PizzaService } from './pizza.service';
+import { DialogComponent, DialogService } from 'ng2-bootstrap-modal';
+//import { AlertComponent } from './alert.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+ 
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app';  
+  formVisible = false;
+  datosTraidos;
+  promptMessage:string = '';
+
+  
+  constructor(private PizzaService : PizzaService, public formBuilder: FormBuilder,private dialogService:DialogService)
+   {    
+    this.TraerPizza();
+   
+  }
+
+
+  TraerPizza(){
+      this.PizzaService.TraerPizza()
+      .then(data => {
+        this.datosTraidos = data;
+      }) 
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+/*showAltaPrompt() {
+    this.dialogService.addDialog( {
+      title:'Alta Usuario',
+      estado:true,
+      nombre:'',
+      apellido:'',
+      sexo:'',
+      dni:0,
+      pass:'',
+       id:0,
+      foto:"http://localhost:80/API/fotos/999999.png" 
+      })   
+      .subscribe((message)=>{
+        this.TraerPizza();
+        //this.promptMessage = message;
+      });
+
+}*/
 }
